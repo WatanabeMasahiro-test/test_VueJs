b4f5149beef572e3635d566590aa4adbd8144e90
WatanabeMasahiro <2c.nave.stk@gmail.com>
2023-04-05 14:06:06 +0900

    second commit(Vueファルダ追加)

--- public/js/test.js
+++ public/js/test.js
@@ -16,12 +16,21 @@ $(function() {
 const app = {
     data() {
         return {
-            message: 'HelloWorld!'
+            message : 'HelloWorld!',
+            message2: 'HELLO',
+            ok      : true
         }
     },
     methods: {
-        reverseMessage: function() {
+        reverseMessage  : function() {
             this.message = this.message.split('').reverse().join('')
+        },
+        sayHi           : function() {
+            return 'Hi!!!';
+        },
+        returnMsg       : function() {
+            this.message2 = 'HelloVueJS';
+            return this.message2 + '___' + this.sayHi();
         }
     }
 }
