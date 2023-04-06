b90660acd9a42461afde642619cbd71618559ae7
WatanabeMasahiro <2c.nave.stk@gmail.com>
2023-04-06 19:07:41 +0900

    ライフサイクルフックまで終了

--- public/section4/js/section4.js
+++ public/section4/js/section4.js
@@ -64,7 +64,7 @@ const vm3 = Vue.createApp({
 const vm4 = Vue.createApp({
     data() {
         return {
-            name     : '渡辺 太郎',
+            name     : '渡辺 サブ郎',
         }
     },
     render() {
@@ -74,4 +74,46 @@ const vm4 = Vue.createApp({
     }
 }).mount('#app4');
 
-var dir = document.createElement('div');
+
+const vm5 = Vue.createApp({
+    data() {
+        return {
+            name : '渡辺 英五郎',
+        }
+    },
+    beforeCreate()  {
+        return console.log('beforeCreate');
+    },
+    create()  {
+        return console.log('created');
+    },
+    beforeMount()  {
+        return console.log('beforeMount');
+    },
+    mounted()  {
+        return console.log('mounted');
+    },
+    beforeUpdate()  {
+        return console.log('beforeUpdate');
+    },
+    updated()  {
+        return console.log('updated');
+    },
+    /**
+     * Vue3で、beforeDestroyとdestroyedは使えない
+     */
+    // beforeDestroy()  {
+    //     return console.log('beforeDestroy');
+    // },
+    // destroyed()  {
+    //     return console.log('destroyed');
+    // },
+    methods     : {
+        nameChange : function() {
+            this.name = "名無しに変更した";
+        },
+        // destroy     : function() {
+        //     this.$unmounted();
+        // }
+    }
+}).mount('#app5');
