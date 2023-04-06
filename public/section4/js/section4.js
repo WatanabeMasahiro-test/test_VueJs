c4d064f8104a70a8ae9ebcff7f16c98becbb30e4
WatanabeMasahiro <2c.nave.stk@gmail.com>
2023-04-06 18:05:20 +0900

    templateプロパティとrender関数

--- public/section4/js/section4.js
+++ public/section4/js/section4.js
@@ -24,6 +24,7 @@ const vm1 =  Vue.createApp({
 
 // vm1.msg = 'CHANGE!!!';
 
+
 const vm2 = Vue.createApp({
     data() {
         return {
@@ -35,10 +36,42 @@ const vm2 = Vue.createApp({
         changeMsg : function() {
             this.bool = !this.bool;
             if(this.bool == true) {
-                vm1.msg = 'メッセージの変更';
+                vm1.msg = 'メッセージの変更'; // thisとの違いに注意
             } else {
-                vm1.msg = 'HELLO';
+                vm1.msg = 'HELLO';         // thisとの違いに注意
             }
         }
     }
 }).mount('#app2');
+
+// console.log(vm2);
+// console.log(vm2.msg);
+// console.log(vm2.$data);
+
+
+const vm3 = Vue.createApp({
+    data() {
+        return {
+            name     : '渡辺 太郎',
+        }
+    },
+    template    : '<h1>こんにちは、{{ name }}</h1>',
+    methods     : {
+    }
+}).mount('#app3');
+
+
+const vm4 = Vue.createApp({
+    data() {
+        return {
+            name     : '渡辺 太郎',
+        }
+    },
+    render() {
+        return Vue.h('h1', 'こんにちは、' + this.name);
+    },
+    methods     : {
+    }
+}).mount('#app4');
+
+var dir = document.createElement('div');
